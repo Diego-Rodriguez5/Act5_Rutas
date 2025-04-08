67162feb4dc0194bab117556dc17f0a5a00b7b53
DIEGO_JESUS_RODRIGUEZ_CHAVEZ <a.22308051281296@cbtis128.edu.mx>
2025-04-08 16:14:18 +0000

    enviar

---
+++ lib/pagina_dos.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:diego/main.dart';
+
+class PantallaDos extends StatelessWidget {
+  const PantallaDos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Pantalla Dos',
+          style: TextStyle(
+            color: Colors.black, // Color de letra blanco
+            fontFamily: 'Times New Roman', // Fuente Times New Roman
+            fontSize: 25, // Tamaño de letra 15
+          ),
+        ),
+        backgroundColor: Color(0xff595897),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Regresar!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pagina_tres.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:diego/main.dart';
+
+class PantallaTres extends StatelessWidget {
+  const PantallaTres({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Pantalla Tres',
+          style: TextStyle(
+            color: Colors.black, // Color de letra blanco
+            fontFamily: 'Times New Roman', // Fuente Times New Roman
+            fontSize: 25, // Tamaño de letra 15
+          ),
+        ),
+        backgroundColor: Color(0xff558698),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Diego'),
+        ),
+      ),
+    );
+  }
+}
